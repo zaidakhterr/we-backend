0d137911179d51e05b8822dc093637e6d20021c3
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-25 11:04:19 +0500

    📦 NEW: Initial Commit

---
+++ src/handler/hello.js
@@ -0,0 +1,8 @@
+async function hello(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from http://zenveus.com" }),
+  };
+}
+
+export const handler = hello;
